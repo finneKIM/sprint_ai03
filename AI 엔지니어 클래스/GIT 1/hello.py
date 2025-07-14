295101da0604156a1d669e2b11e689fce83c2874
김하나 <hana1356kim@gmail.com>
2025-07-14 11:14:32 +0900

    Add initial hello.py

--- AI 엔지니어 클래스/GIT 1/hello.py
+++ AI 엔지니어 클래스/GIT 1/hello.py
@@ -1,6 +1,6 @@
-a = "hello Python"
-print(a)
-print("This is a simple Pytohg script.")
+print(hello Python)
+print("This is a simple Pyton script.")
+print("Feel free to modity this script as you wish.")
 
 
 

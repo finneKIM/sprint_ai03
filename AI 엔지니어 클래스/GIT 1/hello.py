f6531e9ec35f135ee3134afb0a2ace265a2751e4
김하나 <hana1356kim@gmail.com>
2025-07-14 11:27:09 +0900

    add hello.py

--- AI 엔지니어 클래스/GIT 1/hello.py
+++ AI 엔지니어 클래스/GIT 1/hello.py
@@ -1,7 +1,6 @@
-print(hello Python)
+print("hello Python")
 print("This is a simple Pyton script.")
 print("Feel free to modity this script as you wish.")
-
-
+print("Remember to save your change")
 
 
